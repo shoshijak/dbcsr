3d3a715b836e359edb82e91861f7e643ae9f4c41
shoshijak <as.jak@protonmail.ch>
2019-07-24 17:37:51 +0200

    ROCm/HIP backend: initial commit

---
+++ src/acc/hip/acc_hip_dev.cpp
@@ -0,0 +1,62 @@
+/*------------------------------------------------------------------------------------------------*
+ * Copyright (C) by the DBCSR developers group - All rights reserved                              *
+ * This file is part of the DBCSR library.                                                        *
+ *                                                                                                *
+ * For information on the license, see the LICENSE file.                                          *
+ * For further information please visit https://dbcsr.cp2k.org                                    *
+ * SPDX-License-Identifier: GPL-2.0+                                                              *
+ *------------------------------------------------------------------------------------------------*/
+
+#include <hip/hip_runtime.h>
+#include <stdio.h>
+#include <math.h>
+#include "acc_cuda_error.h"
+#include "../include/acc.h"
+
+// for debug purpose
+static const int verbose_print = 1;
+
+/****************************************************************************/
+extern "C" int acc_get_ndevices(int *n_devices){
+  hipError_t cErr;
+
+  cErr = hipGetDeviceCount (n_devices);
+  if (cuda_error_check (cErr))
+    return -1;
+  return 0;
+}
+
+
+/****************************************************************************/
+extern "C" int acc_set_active_device(int device_id){
+  hipError_t cErr;
+  int myDevice, runtimeVersion;
+
+  cErr = cudaRuntimeGetVersion(&runtimeVersion);
+  if (cuda_error_check (cErr))
+    return -1;
+
+  cErr = hipSetDevice (device_id);
+  if (cuda_error_check (cErr))
+    return -1;
+
+  cErr = hipGetDevice (&myDevice);
+  if (cuda_error_check (cErr))
+    return -1;
+
+  if (myDevice != device_id)
+    return -1;
+
+  // establish context
+  cErr = hipFree(0);
+  if (cuda_error_check (cErr))
+    return -1;
+
+  if (verbose_print){
+    cErr = cudaDeviceSetLimit(cudaLimitPrintfFifoSize, (size_t) 1000000000);
+    if (cuda_error_check (cErr))
+      return -1;
+  }
+
+  return 0;
+}
